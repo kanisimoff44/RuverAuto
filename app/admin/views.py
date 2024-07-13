fec6159fade0c552f8d0048679136762b98c6bec
kanisimoff <k.anisimov@part-place.ru>
2024-07-13 23:26:38 +0700

    chore(name): update name and name_plural

--- app/admin/views.py
+++ app/admin/views.py
@@ -5,7 +5,7 @@ from app.products.models import Products
 
 class ProductsAdmin(ModelView, model = Products):
     column_list = [c.name for c in Products.__table__.c]
-    name = "Авто"
-    name_plural = "Автомобили"
+    name = "Товар"
+    name_plural = "Товары"
     icon = "fa-solid fa-car"
     
