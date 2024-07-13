0c86399ab623bf32ab121777154636cb67504267
kanisimoff <k.anisimov@part-place.ru>
2024-07-13 23:39:42 +0700

    feature(dao): add data access object

---
+++ app/dao/base.py
@@ -0,0 +1,14 @@
+from sqlalchemy import select
+
+from app.database import async_session_maker
+
+
+class BaseDAO:
+    model = None
+    
+    @classmethod
+    async def get_all(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.scalars().all()
