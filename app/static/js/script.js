0583fcd334f0b8180b9601ef72e8bc0bb15412dc
kanisimoff <k.anisimov@part-place.ru>
2024-10-02 11:38:52 +0700

    refactor: :recycle: the layout of the footer

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const priceElement = document.querySelector('.price');
-  const priceValue = priceElement.textContent.replace('₽', '').trim(); // Убираем знак рубля
-  const formattedPrice = Number(priceValue).toLocaleString('ru-RU'); // Форматируем число
-  priceElement.textContent = formattedPrice + ' ₽'; // Добавляем пробел перед рублем
+  const priceElements = document.querySelectorAll('.price'); // обработка всех элементов с классом price
+  priceElements.forEach(priceElement => {
+    let priceValue = priceElement.textContent.replace('₽', '').trim();
+
+    if (!isNaN(priceValue) && priceValue !== "") { // провека на число
+      const formattedPrice = Number(priceValue).toLocaleString('ru-RU'); // форматирование
+      priceElement.textContent = formattedPrice + ' ₽';
+    } else {
+      console.warn("Некорректное значение цены:", priceValue); // вывод предупреждения в консоль для отладки
+    }
+  });
 });
